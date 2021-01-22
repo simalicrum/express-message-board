3df09ac20d0d164984f3a49c4b85e1e782f21783
simalicrum <robert.reinert@gmail.com>
2021-01-22 10:41:06 -0800

    display messages array

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = router;
